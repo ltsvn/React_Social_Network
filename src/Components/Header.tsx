94cc4c1a374746280faf154a728d99ce14f4edf3
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-08-21 16:04:11 +0200

    create components

---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTj-MM_UyJqsvN2GeGKQKLEHNaL-cfeoLa-Q&usqp=CAU'/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
+            </div>
+            <div>
+                ava + discription
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
