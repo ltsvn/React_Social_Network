fe405b6cb1fa0b25fb70f14b195fd4de900956ff
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-08-21 20:51:35 +0200

    create Posts

---
+++ src/Components/Profile/MyPost/Post/Post.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from "./Post.module.css";
+
+
+const Post = () => {
+    return (
+        <div className={s.item}>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI8AeBnWElMUXOK6cBZbyvgqj0hdHdSs9S-g&usqp=CAU'/>
+            post 1
+            <div>
+                <span>like</span>
+            </div>
+        </div>
+    )
+}
+
+export default Post;
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import s from './Profile.module.css'
+import MyPosts from "./MyPost/MyPosts";
+
 
 const Profile = () => {
     return (
@@ -11,20 +13,7 @@ const Profile = () => {
             <div>
                 ava + discription
             </div>
-            <div>
-                my posts
-                <div>
-                    new post
-                </div>
-                <div className={s.posts}>
-                    <div className={s.item}>
-                        post 1
-                    </div>
-                    <div className={s.item}>
-                        post 2
-                    </div>
-                </div>
-            </div>
+            <MyPosts />
         </div>
     )
 }
