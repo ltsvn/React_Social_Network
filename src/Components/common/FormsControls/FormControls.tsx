bd1f5bf9ba2267e307a25b1854fb3b1f3decf1de
ltsvn <92997990+ltsvn@users.noreply.github.com>
2023-05-22 18:30:45 +0200

    minor changes

--- src/Components/common/FormsControls/FormControls.tsx
+++ src/Components/common/FormsControls/FormControls.tsx
@@ -23,13 +23,13 @@ const FormControl: FC<TextareaProps> = ({input, meta, children, ...props}) =>{
         </div>
     );
 }
-//
-// export const Textarea = (props) => {
-//     const {input, meta, children, ...restProps} = props;
-//    return <FormControl {...props}><textarea {...input} {...restProps} /></FormControl>
-// };
-//
-// export const Input = (props) => {
-//     const {input, meta, children, ...restProps} = props;
-//     return <FormControl {...props}><input {...input} {...restProps} /></FormControl>
-// };
+
+export const Textarea = (props) => {
+    const {input, meta, children, ...restProps} = props;
+   return <FormControl {...props}><textarea {...input} {...restProps} /></FormControl>
+};
+
+export const Input = (props) => {
+    const {input, meta, children, ...restProps} = props;
+    return <FormControl {...props}><input {...input} {...restProps} /></FormControl>
+};
